ef21f0f39cc58631e195a8849f9eed3a70961f8a
ilumivision <ilumivision3@gmail.com>
2026-06-29 20:10:55 -0700

    index.js

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ app.use(express.json());
 
 const VERIFY_TOKEN = "BHOOMIMITRA2026";
 
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxlgnztAqKZVihwYoUCwkJdJIOTLCVUiZnQyAuTxMLO84aPy9GnC56D5TU4MNe1-5XQqw/exec";
+
 app.get("/", (req, res) => {
     res.send("BhoomiMitra AI Server Running");
 });
 
+// WhatsApp Webhook Verification
 app.get("/webhook", (req, res) => {
 
     const mode = req.query["hub.mode"];
@@ -21,21 +24,5 @@ app.get("/webhook", (req, res) => {
     }
 
     return res.sendStatus(403);
-
 });
 
-app.post("/webhook", (req, res) => {
-
-    console.log("Incoming WhatsApp message");
-
-    console.log(JSON.stringify(req.body, null, 2));
-
-    res.sendStatus(200);
-
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server started on port " + PORT);
-});
