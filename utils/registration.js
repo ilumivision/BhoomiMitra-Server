2cb6565de157e2134ee254f5e32dacde7ba0c12a
ilumivision <ilumivision3@gmail.com>
2026-07-06 11:50:19 -0700

    Add registration module

---
+++ utils/registration.js
@@ -0,0 +1,8 @@
+async function registrationModule(data) {
+    return {
+        success: true,
+        module: "REGISTRATION",
+        reply: "Registration module is working."
+    };
+}
+module.exports = registrationModule;
