8cb6e4fd17fdb4163b857ab441d596c5684dc14b
ilumivision <ilumivision3@gmail.com>
2026-07-06 11:55:55 -0700

    Add voice module

---
+++ utils/voice.js
@@ -0,0 +1,8 @@
+async function voiceModule(data) {
+    return {
+        success: true,
+        module: "VOICE",
+        reply: "Voice module is working."
+    };
+}
+module.exports = voiceModule;
