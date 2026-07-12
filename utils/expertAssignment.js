41379fd37f4a316b7c7442e002bd370acf08c064
ilumivision <ilumivision3@gmail.com>
2026-07-12 03:42:58 -0700

    Test full Expert Directory range

--- utils/expertAssignment.js
+++ utils/expertAssignment.js
@@ -314,9 +314,9 @@ async function assignExpertCase(options) {
   if (typeof appendRow !== "function") {
     throw new Error("appendRow function is required.");
   }
-  const expertRows = await readSheetRows(
+ const expertRows = await readSheetRows(
   "Expert_Directory",
-  "A2:AS"
+  "A:AS"
 );
     const problemCategory =
     caseData.problemCategory ||
