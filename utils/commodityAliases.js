e91b9006cea24fa5b8d5fddf7eebf0c8d27ad53b
ilumivision <ilumivision3@gmail.com>
2026-07-16 10:56:51 -0700

    Create commodity alias engine
    Added commodity aliases for various items.

---
+++ utils/commodityAliases.js
@@ -0,0 +1,50 @@
+"use strict";
+module.exports = {
+  coconut: [
+    "Coconut",
+    "Tender Coconut",
+    "Coconut Dry"
+  ],
+  "black pepper": [
+    "Black Pepper",
+    "Pepper",
+    "Black pepper"
+  ],
+  ginger: [
+    "Ginger",
+    "Green Ginger",
+    "Ginger (Green)"
+  ],
+  turmeric: [
+    "Turmeric"
+  ],
+  banana: [
+    "Banana",
+    "Nendran",
+    "Plantain"
+  ],
+  rubber: [
+    "Rubber",
+    "Latex",
+    "Rubber Sheet"
+  ],
+  arecanut: [
+    "Arecanut",
+    "Betel Nut"
+  ],
+  cocoa: [
+    "Cocoa"
+  ],
+  nutmeg: [
+    "Nutmeg"
+  ],
+  mango: [
+    "Mango"
+  ],
+  pineapple: [
+    "Pineapple"
+  ],
+  jackfruit: [
+    "Jackfruit"
+  ]
+};
