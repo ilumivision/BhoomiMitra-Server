d139cbde81ad5469f92ebd26c199c12bd8a8414b
ilumivision <ilumivision3@gmail.com>
2026-08-04 04:44:48 -0700

    Connect land registration module

--- utils/landRegistration.js
+++ utils/landRegistration.js
@@ -2,6 +2,15 @@
 
 async function registerLand(data) {
 
+  console.log("Registering land...");
+
+  console.log(data);
+
+  return {
+    success: true,
+    landId: "LAND-TEMP"
+  };
+
 }
 
 module.exports = {
