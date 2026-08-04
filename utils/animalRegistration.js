834611c3161bbe48ad0e9221d5566a0896cdef8b
ilumivision <ilumivision3@gmail.com>
2026-08-04 11:31:44 -0700

    fix: correct animal registry header regex

--- utils/animalRegistration.js
+++ utils/animalRegistration.js
@@ -7,7 +7,7 @@ function normalizeHeader(value) {
     .trim()
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, "_")
-    .replace(/^+|+$/g, "");
+    .replace(/^_+|_+$/g, "");
 }
 
 function normalizePhone(value) {
